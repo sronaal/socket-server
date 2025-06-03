e824d0782fa5c387cb41da9d04dcb412905da63b
Ronal <ronal.sanx21@gmail.com>
2025-06-03 14:23:30 -0500

    configuracion cors

--- models/server.js
+++ models/server.js
@@ -2,6 +2,8 @@ const express = require("express")
 const http  = require("http")
 const socketio = require("socket.io")
 const path = require('path');
+const cors = require("cors")
+
 const Sockets = require("./sockets");
 
 
@@ -25,7 +27,7 @@ class Server{
 
     middlewares(){
         this.app.use(express.static(path.resolve(__dirname, '../public')))
-
+        this.use(cors())
     }
 
     configurarSocket(){
